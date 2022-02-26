775db9eefce0d97c5f81826af83c11e5cb565272
Arjoon <pertauba@gmail.com>
2022-02-26 09:29:19 +0400

    first commit

---
+++ democeridian/src/main/java/com/ceridian/demo/democeridian/DemoceridianApplication.java
@@ -0,0 +1,13 @@
+package com.ceridian.demo.democeridian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoceridianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoceridianApplication.class, args);
+	}
+
+}
---
+++ democeridian/src/test/java/com/ceridian/demo/democeridian/DemoceridianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceridian.demo.democeridian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoceridianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
